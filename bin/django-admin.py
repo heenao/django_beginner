b4c842e20a460491f0edeeb8a2e03b5db0a31564
Heena <heena.otia@brealtime.com>
2017-07-18 12:37:28 -0400

    test

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/heena.otia/Django_files/django_beginner/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ greetings/urls.py
@@ -0,0 +1,8 @@
+# greetings/urls.py
+from django.conf.urls import url
+
+from .views import index
+
+urlpatterns = [
+    url(r'^$', index),
+]
